382597a67175dfa3e8c017409d9e6b26539fc171
Hira3th <cedric-98@hotmail.co.uk>
2025-04-12 12:09:12 +0200

    Refactor Subject class and update namespaces
    Moved the Subject class to the Observer.Logic namespace and restored necessary using directives. Updated Program.cs to remove unnecessary casting and added comments for clarity. Improved code organization and readability while maintaining existing functionality.

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,19 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Observer.Models;
+﻿using Observer.Models;
+using Observer.Logic;
 
 Console.WriteLine("Hello, Observer!");
 
+// Create the subject (the entity being observed)
 Subject subject = new Subject();
 
-
+// Create observers (entities that will react to changes in the subject)
 Observer.Models.Observer observer1 = new Observer.Models.Observer("Observer 1");
 Observer.Models.Observer observer2 = new Observer.Models.Observer("Observer 2");
 
-subject.Attach((IObserver)observer1);
-subject.Attach((IObserver)observer2);
+// Attach observers to the subject
+subject.Attach(observer1);
+subject.Attach(observer2);
 
+// Notify all attached observers of an update
 subject.Notify("Update available");
 
-subject.Detach((IObserver)observer1);
+// Detach one observer
+subject.Detach(observer1);
 
+// Notify remaining observers of another update
 subject.Notify("Another update");
