2aec547038f7c7442697da5df30fd39fdf4c309d
Hira3th <cedric-98@hotmail.co.uk>
2025-04-12 10:24:49 +0200

    Add CommandPattern2 project implementing Command pattern
    This commit introduces a new C# project named `CommandPattern2`, featuring a console application that demonstrates the Command design pattern. Key components include:
    
    - `ICommand` interface defining the command contract.
    - `SimpleCommand` and `ComplexCommand` classes implementing the command functionality.
    - `Invoker` class to manage command execution.
    - `Receiver` class to perform specific actions based on commands.
    
    The project is set up for .NET 8.0 and includes the necessary project and solution files for Visual Studio.

---
+++ CommandPattern2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CommandPattern2.Logic;
+
+Console.WriteLine("Hello, World!");
+
+// The client code can parameterize an invoker with any commands.
+Invoker invoker = new Invoker();
+invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+Receiver receiver = new Receiver();
+invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+invoker.DoSomethingImportant();
